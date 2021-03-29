1c8c68df13ae65db920882a7e043fb6912df83a1
sergiughimpExpleo <78013000+sergiughimpExpleo@users.noreply.github.com>
2021-03-29 14:48:21 +0300

    Contact List

---
+++ contactlist/contact/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import redirect, render
+from .models import Contact
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def addContact(request):
+    if request.method == 'POST':
+        new_contact = Contact(
+            full_name = request.POST['fullname'],
+            relationship = request.POST['relationship'],
+            email = request.POST['email'],
+            phone_number = request.POST['phone-number'],
+            address = request.POST['address']
+        )
+        new_contact.save()
+        return redirect('/')
+    return render(request, 'new.html')
