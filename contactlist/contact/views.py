da683dc13416b67c253d873d68d372b3d361e87b
sergiughimpExpleo <78013000+sergiughimpExpleo@users.noreply.github.com>
2021-04-05 17:22:01 +0300

    Integrating Search Feature

--- contactlist/contact/views.py
+++ contactlist/contact/views.py
@@ -4,8 +4,13 @@ from .models import Contact
 
 def index(request):
     contacts = Contact.objects.all()
-    
-    return render(request, 'index.html', {'contacts':contacts})
+    search_input =request.GET.get('search-area')
+    if  search_input:
+        contacts = Contact.objects.filter(full_name__icontains=search_input)
+    else:
+        contacts = Contact.objects.all()
+        search_input = '' 
+    return render(request, 'index.html', {'contacts':contacts, 'search_input':search_input })
 
 def addContact(request):
     if request.method == 'POST':
